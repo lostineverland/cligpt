f5b70944faad2a0d0b2f6d69e0d8e18111e1a41f
Carlos A. Bonilla <lostineverland@gmail.com>
2024-03-20 22:42:22 -0400

    switch the config file to JSON to include more default values.

--- cligpt.py
+++ cligpt.py
@@ -4,6 +4,10 @@ import json, argparse
 import os, datetime
 import urllib.request
 import urllib.error
+from collections import namedtuple
+
+# turns a dict into an object
+objdict = lambda d: namedtuple('objdict', d.keys())(**d)
 
 def callgpt(messages, model, api_key):
     endpoint = 'https://api.openai.com/v1/chat/completions'
@@ -37,14 +41,6 @@ def iso_minute():
     ISO_8601_MINUTES = '%Y-%m-%dT%H-%M'
     return datetime.datetime.now().strftime(ISO_8601_MINUTES)
 
-def get_key():
-    keypath = os.path.join(os.getenv('HOME'), '.cligpt')
-    try:
-        with open(keypath) as f:
-            return f.read().rstrip()
-    except:
-        raise Exception(f'did not find the API key at: {keypath}')
-
 def input_block(prompt):
     block = ''
     line = input(prompt)
@@ -69,12 +65,38 @@ def log_interaction(log, query, answer):
     log.flush()
     print('\n' + rendered_a, flush=True)
 
-def enter_query_loop(args, query):
-    api_key = get_key()
-    os.makedirs(os.path.join(os.getcwd(), 'GPT_logs', iso_year(), iso_month()), exist_ok=True)
-    messages = [{"role": "system", "content": args.role}]
-    model = args.model
-    log_path = os.path.join('GPT_logs', iso_year(), iso_month(), '{}.md'.format(iso_minute()))
+def setpath(config):
+    log_path = config.get('log_path', 'GPT_logs')
+    os.makedirs(os.path.join(os.getcwd(), log_path, iso_year(), iso_month()), exist_ok=True)
+    return log_path
+
+def set_default_config(config_path):
+    with open(config_path, 'x') as f:
+        json.dump(dict(
+            api_key='sk-################################################',
+            log_path='GPT_logs',
+            model='gpt-4',
+            role='You are a Google Search replacement',
+            ), f, indent=4)
+    raise Exception('Please set configuration $HOME/.cligpt')
+
+def get_config():
+    config_path = os.path.join(os.getenv('HOME'), '.cligpt')
+    default_api_key = 'sk-################################################'
+    try:
+        with open(config_path) as f:
+            config = json.load(f)
+        if config.get('api_key', default_api_key) == default_api_key:
+            raise Exception(f'Please set the API key at: {config_path}')
+        return config
+    except FileNotFoundError:
+        set_default_config(config_path)
+
+def enter_query_loop(args, query, config):
+    api_key = config.get('api_key')
+    log_path = os.path.join(setpath(config), iso_year(), iso_month(), '{}.md'.format(iso_minute()))
+    messages = [{"role": "system", "content": args.role or config.get('role', 'You are a knowledge engine')}]
+    model = args.model or config.get('model', 'gpt-4')
     with open(log_path, 'w') as log:
         while query:
             messages += [dict(role='user', content=query)]
@@ -88,17 +110,18 @@ def enter_query_loop(args, query):
 def cli_parser():
     '''CLI tools'''
     parser = argparse.ArgumentParser(description='This runs OpenAI GPT from the terminal. Press CTRL-C to exit.')
-    parser.add_argument('role', nargs='?', default="You are a Google Search replacement",
-        help='Describe What kind of agent/helper you want, (default: "You are a Google Search replacement")')
-    parser.add_argument('-m', '--model', dest='model', default="gpt-4", help='Which model ie "gpt-4-turbo-preview"')
+    parser.add_argument('role', nargs='?', default=None,
+        help='Describe What kind of agent/helper you want, (default is set in $HOME/.cligpt)')
+    parser.add_argument('-m', '--model', dest='model', default=None, help='Which model ie "gpt-4-turbo-preview"')
     return parser.parse_args()
 
 def main():
+    config = get_config()
     args = cli_parser()
     print('args:', args)
     query = input_block(f"{args.model}:\n")
     if query:
-        enter_query_loop(args, query)
+        enter_query_loop(args, query, config)
 
 if __name__ == '__main__':
     main()
